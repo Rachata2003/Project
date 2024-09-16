2254b1ac6b0128559ccdc91e9730bc16e83a9fd5
2003K <rachatask139@gmail.com>
2024-09-16 11:46:08 +0700

    folder_rearrange1

--- data_collection/insertformpg2.php
+++ data_collection/insertformpg2.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $suffix; ?> Time Email</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
     <div class="e1_2">
